3e3e14d369273a792ff98f9427586924d1816b18
venom_1404 <ronitdhase@gmail.com>
2024-12-20 10:23:46 +0530

    docmodel

---
+++ server/models/doctormodel.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true,
+    },
+    email: {
+        type:String,
+        required: true
+    },
+    password: {
+        type:String,
+        required:true
+    },
+    speciality: {
+        type:String,
+        required:true
+    },
+    degree: {
+        type:String,
+        required: true
+    },
+    experience: {
+        type:String,
+        required:true
+    },
+    about: {
+        type: String,
+        required: true
+    },
+    available: {
+        type: Boolean,
+        required: true
+    },
+    fees: {
+        type: Number,
+        required:true
+    },
+    address: {
+        type: Object,
+        required: true
+    },
+    date: {
+        type: Number,
+        required: true
+    },
+    slots_booked: {
+        type: Object,
+        default:{}
+    }
+},{minimize:false}) //we add ,{minimize:false} here so that we can store empty object in data
+
+const doctorModel = mongoose.models.doctor || mongoose.model('doctor',doctorSchema)
+
+export default doctorModel
