735455b9ce70d190d96fc616cf4c744654d6d1cd
venom_1404 <ronitdhase@gmail.com>
2025-01-01 09:56:53 +0530

    updates

--- client/src/components/TopDoctors.jsx
+++ client/src/components/TopDoctors.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import { doctors } from '../assets/assets_frontend/assets.js'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const TopDoctors = () => {
+
+  const navigate = useNavigate()
+  const {doctors} = useContext(AppContext)
+
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
       <h1 className='text-3xl font-medium'>Top Doctors</h1>
@@ -9,7 +14,13 @@ const TopDoctors = () => {
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {doctors.slice(0,10).map((item,index) => {
           return(
-            <div className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 ' key={index}>
+            <div onClick={() => {
+              if (item._id) {
+                navigate(`/appointment/${item._id}`);
+              } else {
+                console.error('Doctor ID is missing', item);
+              }
+            }} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 ' key={index}>
               <img className='bg-blue-50' src={item.image} alt="" />
               <div className='p-4'>
                 <div className='flex items-center gap-2 text-sm text-center text-green-500'>
@@ -22,7 +33,7 @@ const TopDoctors = () => {
           )
         })}
       </div>
-      <button className='bg-blue-200 font-medium text-gray-900 px-12 py-3 rounded-full mt-10'>more</button>
+      <button onClick={() => {navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-200 font-medium text-gray-900 px-12 py-3 rounded-full mt-10'>more</button>
     </div>
   )
 }
